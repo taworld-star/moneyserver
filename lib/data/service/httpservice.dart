0f6a8f5a83fe830d120c3ef3ea2a05104c3ed5df
taworld-star <sustrisimamora0413@gmail.com>
2026-01-06 08:33:16 +0700

    Update HTTP service base URL for physical device

--- lib/data/service/httpservice.dart
+++ lib/data/service/httpservice.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 
 class HttpService {
   final String baseUrl =
-      "http://192.168.1.17:8000/api/"; // For physical device with WiFi
+      "http://10.16.242.164:8000/api/"; // For physical device with WiFi
   // final String baseUrl = "http://10.0.2.2:8000/api/"; // For Android emulator
   // final String baseUrl = "http://127.0.0.1:8000/api/"; // For Flutter Web
 
